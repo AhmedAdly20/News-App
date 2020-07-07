83261edfb4e489bd93f547d47ed950b6df2a9809
AhmedAdly20 <ahmedadly132@gmail.com>
2020-07-07 11:41:16 +0200

    Popular Tab Screen

--- lib/screens/home_tabs/Popular.dart
+++ lib/screens/home_tabs/Popular.dart
@@ -8,8 +8,63 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
+    return ListView.builder(
+      itemBuilder: (context,position){
+        return Card(
+          child: _drawSingleRow(),
+        );
+      },
+      itemCount: 20,
     );
   }
+
+
+  Widget _drawSingleRow() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            child: Image(
+              image: ExactAssetImage('assets/images/placeholder_bg.png'),
+              fit: BoxFit.cover,
+            ),
+            width: 125.0,
+            height: 125.0,
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'The World Global Warming Annual Summit',
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 18.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Michael Adams'),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.timer),
+                        Text('15 min'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
 }
