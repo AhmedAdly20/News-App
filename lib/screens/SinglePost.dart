56135ea2674403baf655472b0e921e83abc374c0
AhmedAdly20 <ahmedadly132@gmail.com>
2020-07-14 19:35:13 +0200

    SinglePost Sliver

--- lib/screens/SinglePost.dart
+++ lib/screens/SinglePost.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:newsapp/models/Post.dart';
 
 class SinglePost extends StatefulWidget {
   final Post post;
+
   SinglePost(this.post);
+
   @override
   _SinglePostState createState() => _SinglePostState();
 }
@@ -11,11 +14,49 @@ class SinglePost extends StatefulWidget {
 class _SinglePostState extends State<SinglePost> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.tealAccent,
-      ),
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+            expandedHeight: 300.0,
+            floating: true,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(widget.post.featuredImage),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, position) {
+            if( position == 0 ){
+              return Container(
+                height: 500,
+                color: Colors.teal,
+              );
+            }else{
+              return Container(
+                height: 250,
+                color: getRandomColor(),
+              );
+            }
+
+          }, childCount: 25),
+        ),
+      ],
+    );
+  }
+
+  Color getRandomColor({int minBrightness = 50}) {
+    final random = Random();
+    assert(minBrightness >= 0 && minBrightness <= 255);
+    return Color.fromARGB(
+      0xFF,
+      minBrightness + random.nextInt(255 - minBrightness),
+      minBrightness + random.nextInt(255 - minBrightness),
+      minBrightness + random.nextInt(255 - minBrightness),
     );
   }
 }
